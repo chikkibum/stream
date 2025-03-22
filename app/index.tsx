605e97a029a4b2320504f6fb9493f51ed2ee57ae
chikkibum <bhaskarpandeycontacts@gmail.com>
2025-03-23 03:37:41 +0530

    starter templates

--- app/index.tsx
+++ app/index.tsx
@@ -16,8 +16,6 @@ const barlowFont = Barlow({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-
-
 export default function Home() {
   return (
     <main className={barlowFont.className}>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NavBar } from "@/components/navbar/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-950 text-zinc-200 selection:bg-zinc-600 `} 
       >
+              <NavBar />
         {children}
       </body>
     </html>
